abe188c663255d89dad007d9027c3eb59e94cc38
annguyen2k8 <hacker.an1901@gmail.com>
2026-04-18 18:04:59 +0700

    Big cleanup, move sprites out, simplify code

--- pydustry.py
+++ pydustry.py
@@ -2,9 +2,7 @@ from __future__ import annotations
 
 import sys
 import time
-from functools import wraps
-from typing import Any, Dict, List, Protocol
-import traceback
+from typing import Any
 
 import jpype
 import jpype.imports
@@ -20,113 +18,14 @@ if not jpype.isJVMStarted():
     )
 
 from arc.files import Fi
-from arc.util.io import Reads
 from mindustry import Vars
 from mindustry.core import ContentLoader, Version
 from mindustry.ctype import ContentType
 from mindustry.game import Schematics
-from java.io import UTFDataFormatException
+from mindustry.content import Blocks, Items, Liquids
 
-
-class _Block(Protocol):
-    name: str
-    size: int
-    rotate: bool
-    sizeOffset: int
-
-class _Color(Protocol):
-    r: float
-    g: float
-    b: float
-    a: float
-
-
-class _Item(Protocol):
-    name: str
-    color: _Color
-
-class _ItemStack(Protocol):
-    item: _Item
-    amount: int = 0
-
-class _Schematic(Protocol):
-    tiles: List[_Stile]
-    labels: List[str]
-    tags: Dict[str, str]
-    width: int
-    height: int
-
-    def powerProduction(self) -> float: ...
-    def powerConsumption(self) -> float: ...
-    def requirements(self) -> List[_ItemStack]: ...
-    def name(self) -> str: ...
-    def description(self) -> str: ...
-
-    class _Stile(Protocol):
-        block: _Block
-        x: int
-        y: int
-        config: object
-        rotation: bytes
-
-class _Reads(Protocol):
-    def checkEOF(self) -> int: ...
-    def l(self) -> int: ...
-    def i(self) -> int: ...
-    def s(self) -> int: ...
-    def b(self) -> bytes: ...
-    def b(self, length: int) -> List[bytes]: ...    
-    def b(self, array: List[bytes]) -> List[bytes]: ...
-    def b(self, array: List[bytes], offset: int, length: int) -> List[bytes]: ...
-    def ub(self) -> int: ...
-    def bool(self) -> bool: ...
-    def f(self) -> float: ...
-    def str(self) -> str: ...
-    ...
-
-class _Fi(Protocol):
-    def child(self, name: str) -> _Fi: ...
-    def parent(self) -> _Fi: ...
-    def reads(self) -> Reads: ...
-    def path(self) -> str: ...
-    def mkdirs(self) -> bool: ...
-    
-
-def memoize(func):
-    cache = {}
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        key = (args, frozenset(kwargs.items()))
-        if key not in cache:
-            cache[key] = func(*args, **kwargs)
-        return cache[key]
-
-    return wrapper
-
-class Sprite:
-    image: _Fi
-    name: str
-    offset_x: float
-    offset_y: float
-    original_width: int
-    original_height: int
-    rotate: bool
-    left: int
-    top: int
-    width: int
-    height: int
-    splits: List[int]
-    pads: List[int]
-    
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({", ".join(f"{k}={v!r}" for k, v in self.__dict__.items())})"
-    
-    @memoize
-    def load_image(self): 
-        return Image.open(str(self.image.path())).crop((self.left, self.top, self.left + self.width, self.top + self.height))
-
-
+from protocols import _Schematic
+from sprites import Sprites
 
 tilesize: int = 32
 assets: str = "./assets/"
@@ -136,7 +35,11 @@ def main(*args: Any) -> int:
     Version.enabled = False
 
     Vars.content = ContentLoader()
-    Vars.content.createBaseContent()
+    
+    # only necessary content
+    Items.load()
+    Liquids.load()
+    Blocks.load()
     
     for ctype in ContentType.all:
         for content in Vars.content.getBy(ctype):
@@ -145,103 +48,42 @@ def main(*args: Any) -> int:
             except:
                 pass
 
+    sprites = Sprites.load(Fi(assets + "sprites/sprites.aatls"))
 
-    pack_file: _Fi = Fi(assets + "sprites/sprites.aatls")
-    image_dir: _Fi = pack_file.parent()
-    sprites: Dict[str, Sprite] = {}
     
-    try:
-        try:
-            read: _Reads = pack_file.reads()
-
-            for b in b"AATLS":
-                if read.b() != b:
-                    raise Exception("Invalid binary header. Have you re-packed sprites?")
-            
-            # discard version
-            read.b()
-            
-            while read.checkEOF() != -1:
-                image: str = str(read.str())
-                file: _Fi = image_dir.child(image)
-                
-                read.s()
-                read.s()
-                
-                for i in range(4):
-                    read.b()
-                
-                
-                for i in range(read.i()):
-                    sprite: Sprite = Sprite()
-                    
-                    sprite.image = file
-                    
-                    sprite.name = str(read.str())
-                    sprite.left = read.s()
-                    sprite.top = read.s()
-                    sprite.width = read.s()
-                    sprite.height = read.s()
-                    
-                    if read.bool():
-                        sprite.offset_x = read.s()
-                        sprite.offset_y = read.s()
-                        sprite.original_width = read.s()
-                        sprite.original_height = read.s()
-                    
-                    if read.bool():
-                        sprite.splits = [read.s(), read.s(), read.s(), read.s()]
-                    
-                    if read.bool():
-                        sprite.pads = [read.s(), read.s(), read.s(), read.s()]
-
-                    sprites[sprite.name] = sprite
-
-        except Exception:
-            raise Exception("Error reading pack file: ", pack_file.path())
-        
-        def get_sprite(name: str) -> Sprite:
-            if name not in sprites:
-                print(f"Missing sprite: \"{name}\"")
-            
-            return sprites.get(name, sprites["error"])
+    schem: _Schematic = Schematics.read(Fi(args[1]))
     
-        schem: _Schematic = Schematics.read(Fi(args[1]))
+    # print(f"{schem.name()=}")
+    # print(f"{schem.description()=}")
+    
+    # print(f"{schem.powerProduction()=}")
+    # print(f"{schem.powerConsumption()=}")
+    
+    # requirements = schem.requirements()
+    
+    # for stack in requirements:
+    #     stack: _ItemStack
+    #     print(stack.item.name, stack.amount)
+    
+    ts = time.time()
+    
+    image: Image.Image = Image.new("RGBA", (schem.width * tilesize, schem.height * tilesize))
+    
+    for tile in schem.tiles:
+        block = tile.block
         
-        # print(f"{schem.name()=}")
-        # print(f"{schem.description()=}")
+        rotate: float = tile.rotation * 90 if block.rotate else 0
+        offset = block.sizeOffset
+        draw_x = int((tile.x + offset) * tilesize)
+        draw_y = int((schem.height - tile.y - block.size - offset) * tilesize)
+        draw_size = block.size * tilesize
         
-        # print(f"{schem.powerProduction()=}")
-        # print(f"{schem.powerConsumption()=}")
+        image.paste(sprites.get_sprite(block.name).image().resize((draw_size, draw_size)).rotate(rotate), (draw_x, draw_y))
         
-        # requirements = schem.requirements()
-        
-        # for stack in requirements:
-        #     stack: _ItemStack
-        #     print(stack.item.name, stack.amount)
-        
-        ts = time.time()
-        
-        image: Image.Image = Image.new("RGBA", (schem.width * tilesize, schem.height * tilesize))
-        
-        for tile in schem.tiles:
-            block = tile.block
-            
-            rotate: float = tile.rotation * 90 if block.rotate else 0
-            offset = block.sizeOffset
-            draw_x = int((tile.x + offset) * tilesize)
-            draw_y = int((schem.height - tile.y - block.size - offset) * tilesize)
-            draw_size = block.size * tilesize
-            
-            image.paste(get_sprite(block.name).load_image().resize((draw_size, draw_size)).rotate(rotate), (draw_x, draw_y ))
-            
-        
-        print("Done in" , f"{round(time.time() - ts)}ms")
-        
-        image.show()
-        
-    except Exception:
-        traceback.print_exc(); return 1
+    
+    print("Done in" , f"{round(time.time() - ts)}ms")
+    
+    image.show()
     
     return 0
 
---
+++ sprites.py
@@ -0,0 +1,103 @@
+from __future__ import annotations
+
+from typing import Dict, List, Optional
+
+from PIL import Image
+
+from protocols import _Fi, _Reads
+from utils import memoize
+
+
+class Sprites:
+    sprites: Dict[str, Sprite]
+    packfile: _Fi
+    imagedir: _Fi
+    
+    def __init__(self, packfile: _Fi, imagedir: _Fi) -> None:
+        self.sprites = {}
+        
+        self.packfile = packfile
+        self.imagedir = imagedir
+        
+        try:
+            read: _Reads = packfile.reads()
+
+            for b in b"AATLS":
+                if read.b() != b:
+                    raise Exception("Invalid binary header. Have you re-packed sprites?")
+            
+            # discard version
+            read.b()
+            
+            while read.checkEOF() != -1:
+                image: str = str(read.str())
+                file: _Fi = imagedir.child(image)
+                
+                read.s()
+                read.s()
+                
+                for i in range(4):
+                    read.b()
+                
+                
+                for i in range(read.i()):
+                    sprite = Sprites.Sprite()
+                    
+                    sprite.imagefile = file
+                    
+                    sprite.name = str(read.str())
+                    sprite.left = read.s()
+                    sprite.top = read.s()
+                    sprite.width = read.s()
+                    sprite.height = read.s()
+                    
+                    if read.bool():
+                        sprite.offset_x = read.s()
+                        sprite.offset_y = read.s()
+                        sprite.original_width = read.s()
+                        sprite.original_height = read.s()
+                    
+                    if read.bool():
+                        sprite.splits = [read.s(), read.s(), read.s(), read.s()]
+                    
+                    if read.bool():
+                        sprite.pads = [read.s(), read.s(), read.s(), read.s()]
+
+                    self.sprites[sprite.name] = sprite
+        except Exception:
+            raise Exception(f"Error reading pack file: {packfile.path()}")
+
+    
+    @staticmethod
+    def load(packfile: _Fi) -> Sprites:
+        return Sprites(packfile, packfile.parent())
+
+    def get_sprite(self, name: str) -> Optional[Sprite]:
+        try:
+            return self.sprites[name]
+        except KeyError:
+            print("Missing sprite name:", name)
+        
+        return self.sprites["error"]
+
+    class Sprite:
+        imagefile: _Fi
+        name: str
+        offset_x: float
+        offset_y: float
+        original_width: int
+        original_height: int
+        rotate: bool
+        left: int
+        top: int
+        width: int
+        height: int
+        splits: List[int]
+        pads: List[int]
+        
+        def __repr__(self) -> str:
+            return f"{self.__class__.__name__}({", ".join(f"{k}={v!r}" for k, v in self.__dict__.items())})"
+        
+        @memoize
+        def image(self): 
+            return Image.open(str(self.imagefile.path())).crop((self.left, self.top, self.left + self.width, self.top + self.height))
